cfaf1cc35425062a2e7238319aabdfdaae8940e8
Vladimír Gorej <gorej@codescale.net>
2016-02-12 14:11:46 +0100

    feat(message): add nice contextual menu for message

--- app/scripts/chat/directives/tzMessageMenu/tzMessageMenu.js
+++ app/scripts/chat/directives/tzMessageMenu/tzMessageMenu.js
@@ -8,7 +8,30 @@
   function tzMessageMenu() {
     return {
       restrict: 'E',
-      templateUrl: 'scripts/chat/directives/tzMessageMenu/tz-message-menu.tpl.html'
+      require: ['^md-list-item', 'tzMessageMenu'],
+      templateUrl: 'scripts/chat/directives/tzMessageMenu/tz-message-menu.tpl.html',
+      link: tzMessageMenuLinker,
+      controller: TzMessageMenuController,
+      controllerAs: 'tzMessageMenu'
     };
+
+    function tzMessageMenuLinker(scope, element) {
+      scope.$on('$mdMenuOpen', function() {
+        element.parents('.tz-message').addClass('active');
+      });
+
+      scope.$on('$mdMenuClose', function() {
+        element.parents('.tz-message').removeClass('active');
+      });
+    }
+  }
+
+  function TzMessageMenuController() {
+    var vm = this;
+    vm.openMenu = openMenu;
+
+    function openMenu($mdOpenMenu, $event) {
+      $mdOpenMenu($event);
+    }
   }
 })();
